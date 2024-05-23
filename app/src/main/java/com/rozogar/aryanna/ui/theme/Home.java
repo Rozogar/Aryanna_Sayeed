42cafdb532861cf85c96abef4588b5f19357a43a
Rozogar <arozogar@gmail.com>
2024-05-23 16:06:15 -0700

    Setting the Home, Song_list class for going ahead.

---
+++ app/src/main/java/com/rozogar/aryanna/ui/theme/Home.java
@@ -0,0 +1,47 @@
+package com.rozogar.aryanna.ui.theme;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.rozogar.aryanna.R;
+
+public class Home extends AppCompatActivity {
+private Button btnSongs;
+private Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        setUpView();
+        btnAction();
+
+    }
+
+    private void btnAction() {
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+        btnSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void setUpView() {
+        btnSongs = findViewById(R.id.btnSongs);
+        btnExit = findViewById(R.id.btnExit);
+    }
+}
--- app/src/main/java/com/rozogar/aryanna/ui/theme/Splash.java
+++ app/src/main/java/com/rozogar/aryanna/ui/theme/Splash.java
@@ -1,26 +1,34 @@
 package com.rozogar.aryanna.ui.theme;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import com.rozogar.aryanna.R;
 
-public class Splash extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import static android.app.ProgressDialog.show;
+
+public class Splash extends AppCompatActivity {
+private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       handler.postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               Intent intent = new Intent(Splash.this, Home.class);
+               startActivity(intent);
+           }
+       }, 2000);
+
     }
+
+
 }
